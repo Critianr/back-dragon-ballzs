575c63f5d0a638a26d0d8fe47eb39f0c995ae7d5
Critianr <88120310+Critianr@users.noreply.github.com>
2023-01-24 05:28:57 -0500

    up

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const morgan = require('morgan');
 const data = require('./base/db.json')
 const fs = require('fs');
 const path = require('path');
-// const imagesRouter = require('./assets');
+const imagesRouter = require('./assets');
 // const cors = require('cors');
 const app = express();
 //configuraciones
 app.set('port', process.env.PORT || 3000);
-
+app.use('/images', imagesRouter);
 
 //middlewares 
 // app.use('/assets', express.static(path.join(__dirname, './assets')));
@@ -29,29 +29,14 @@ app.use(express.urlencoded({extended: false}));
 // app.use(express.static(__dirname + '/public'));
 
 //rutas
-// app.use('/items/', indexRoutes)
-app.get('/assets/:name', (req, res) => {
-    // Obtiene el nombre de la imagen desde la ruta
-    const nombre = req.params.name;
-  
-    // Lee el archivo de imagen
-    fs.readFile(path.join(__dirname, 'assets', name), (err, data) => {
-      if (err) {
-        res.status(404).json({ error: 'Imagen no encontrada' });
-      } else {
-        // Especifica el tipo de contenido como imagen
-        res.contentType('image/png');
-        // Envía los datos de la imagen en la respuesta
-        res.send(data);
-      }
-    });
-  });
-  
+ 
 app.get('/', async (req, res)=>{
     // const characters = await characters.find();
     res.json(data);
     console.log(data)
 });
+app.use('/assets', imagesRouter);
+
 // app.get('/assets', (req, res) => {
 //     res.json({imagen: '/assets'});
 //   });
