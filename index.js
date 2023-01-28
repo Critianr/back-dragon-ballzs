0892704bdecda0b79f06d2115fa9e620f4125d0d
Critianr <calrod350@gmail.com>
2023-01-28 00:29:02 -0500

    up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const data = require('./base/db.json')
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 //configuraciones
 app.set('port', process.env.PORT || 3000);
@@ -9,14 +9,14 @@ app.set('port', process.env.PORT || 3000);
 
 //middlewares 
 app.use(morgan('dev'));
-// app.use(cors());
-// app.use((req, res, next)=>{
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-//  });
+app.use(cors());
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+ });
  
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
