0c48c26024dbe299634dd0fb41a2c19797e98226
Itacgi <calrod350@gmail.com>
2023-06-24 00:17:04 +0200

    hi

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://tu-dominio.com'); // Reemplaza con el dominio de tu aplicación
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 // Indicar que la carpeta "public" es una carpeta estática
 app.use(express.static('public'));
 
