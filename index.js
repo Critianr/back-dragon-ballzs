8e5b75d92776c133cb166185659b9217284e6509
Itacgi <calrod350@gmail.com>
2023-06-25 00:17:57 +0200

    a

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ app.use(function(req, res, next) {
 //   res.setHeader('Access-Control-Allow-Origin', 'https://client-dragon-ballzs.vercel.app/'); // Establece el origen permitido
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Establece los métodos HTTP permitidos
 //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json'); // Establece los encabezados permitidos
+
 res.setHeader('Access-Control-Allow-Credentials', true)
-res.setHeader('Access-Control-Allow-Origin', '*')
+res.setHeader('Access-Control-Allow-Origin', 'https://client-dragon-ballzs.vercel.app/')
 // another common pattern
 // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
