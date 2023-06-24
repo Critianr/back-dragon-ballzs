51ee9e44b678ea559dc947df965f8e0e8435d124
Itacgi <calrod350@gmail.com>
2023-06-24 22:45:44 +0200

    up

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://back-dragon-ballzs-production.up.railway.app/api'); // Reemplaza con el dominio de tu aplicación
+    res.header('Access-Control-Allow-Origin', 'https://back-dragon-ballzs-production.up.railway.app/'); // Reemplaza con el dominio de tu aplicación
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
