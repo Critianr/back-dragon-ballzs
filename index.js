87cb7b9e7698907dbc7ef1424fa35c2f2d9416da
Itacgi <calrod350@gmail.com>
2023-06-25 05:00:27 +0200

    a

--- index.js
+++ index.js
@@ -37,13 +37,12 @@ app.use('/api/assets', express.static(path.join(__dirname, './assets')));
 app.use(morgan('dev'));
 
 
-// app.use((req, res, next)=>{
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-//  });
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();  });
  
 // app.use(cors());
 // app.use((req, res, next)=>{
